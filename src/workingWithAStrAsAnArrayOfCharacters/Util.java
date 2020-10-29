1e7acdfc44f0e2773e322f19ea35895330217b9e
Sauchuk A <owlageone@gmail.com>
2020-10-29 22:01:44 +0300

    util

---
+++ src/workingWithAStrAsAnArrayOfCharacters/Util.java
@@ -0,0 +1,29 @@
+package workingWithAStrAsAnArrayOfCharacters;
+
+import java.util.Scanner;
+
+public class Util {
+
+    static int arrLength;
+
+    static String stringInput() {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        return str;
+    }
+
+    static int arrayInput() {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            arrLength = sc.nextInt();
+            if (arrLength < 2) {
+                System.out.println("Введите число больше чем 2");
+                arrayInput();
+            }
+        } else {
+            System.out.println("Введите целое число");
+            arrayInput();
+        }
+        return arrLength;
+    }
+}
