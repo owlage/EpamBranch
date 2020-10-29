0f508ab4d6f7ff52dd8f636e0f5f808d8962878f
Sauchuk A <owlageone@gmail.com>
2020-10-29 22:05:55 +0300

    camelCase in snake_case

--- src/workingWithAStrAsAnArrayOfCharacters/NumOne.java
+++ src/workingWithAStrAsAnArrayOfCharacters/NumOne.java
@@ -1,4 +1,36 @@
 package workingWithAStrAsAnArrayOfCharacters;
 
+//1. Дан массив названий переменных в camelCase. Преобразовать названия в snake_case.
+
 public class NumOne {
+
+    public static void main(String[] args) {
+        System.out.println("Введите кол-во названий переменых");
+        int arrayLength = Util.arrayInput();// какое кол-во строк
+        String[] array = new String[arrayLength];
+        System.out.println("Введите названия переменых черех enter");
+        //заполняем массив String введеными строками
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Введите низвание переменой " + (i + 1));
+            array[i] = Util.stringInput();
+        }
+        char ch;
+        for (int i = 0; i < array.length; i++) {
+            StringBuilder strBuilder = new StringBuilder();
+            for (int j = 0; j < array[i].length(); j++) {
+                ch = array[i].toCharArray()[j];//символ строки приводим к char
+                //определяем находится ли символ в нижнем регистре если да то возвращаем обновлённый обьект в strBuilder
+                if (Character.isLowerCase(ch) | Character.isDigit(ch)) {
+                    strBuilder.append(ch);
+                } else { //если верхний регитр то возвращаем сначала "_", за тем переводим нижний регистр
+                    strBuilder.append("_");
+                    strBuilder.append(Character.toLowerCase(ch));
+                }
+            }
+            array[i] = strBuilder.toString(); // в массив перезаписываем новые значения
+        }
+        for (String str : array) { // выводим массив array
+            System.out.println(str);
+        }
+    }
 }
