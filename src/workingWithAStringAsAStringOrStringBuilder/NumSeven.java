e25e157d053d058faa3d98d02c99b55d9ce98dbe
Sauchuk A <owlageone@gmail.com>
2020-10-31 00:51:29 +0300

    remove repetitions

---
+++ src/workingWithAStringAsAStringOrStringBuilder/NumSeven.java
@@ -0,0 +1,32 @@
+package workingWithAStringAsAStringOrStringBuilder;
+/*
+7. Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы.
+Например, если было введено "abc cde def", то должно быть выведено "abcdef".
+ */
+
+public class NumSeven {
+
+    public static void main(String[] args) {
+        System.out.println("Введите строку");
+        String str = UtilWorkingStringBuilder.stringInput();
+        StringBuilder stringBuilder = new StringBuilder();
+        //сначала удал все пробелы из строки
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                stringBuilder.append("");
+            } else {
+                stringBuilder.append(str.charAt(i));
+            }
+        }
+        //из строки без пробелов удаляем все повторения
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            for (int j = i + 1; j < stringBuilder.length(); j++) {
+                if (stringBuilder.charAt(i) == stringBuilder.charAt(j)) {
+                    stringBuilder.deleteCharAt(j);
+                    j--;//если символ удален то проверку начинаем с места удаленного символа
+                }
+            }
+        }
+        System.out.println(stringBuilder.toString());
+    }
+}
