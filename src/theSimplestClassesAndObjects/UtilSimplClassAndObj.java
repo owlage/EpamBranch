ee61a35825695b21187fb2077e450924db218725
Sauchuk A <owlageone@gmail.com>
2020-11-07 01:20:31 +0300

    util

--- src/theSimplestClassesAndObjects/UtilSimplClassAndObj.java
+++ src/theSimplestClassesAndObjects/UtilSimplClassAndObj.java
@@ -1,10 +1,13 @@
 package theSimplestClassesAndObjects;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UtilSimplClassAndObj {
 
     static int number;
+    static String str;
 
     static int numberInput(){
         Scanner sc = new Scanner(System.in);
@@ -15,5 +18,16 @@ public class UtilSimplClassAndObj {
             numberInput();
         }
         return number;
+    }
+
+    static String strInput(){
+        Scanner sc = new Scanner(System.in);
+        if(sc.hasNextLine()){
+            str = sc.nextLine();
+            str = str.replaceAll("[^А-Яа-я]","");
+        } else {
+            strInput();
+        }
+        return str;
     }
 }
